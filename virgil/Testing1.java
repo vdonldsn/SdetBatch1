a21aba8551f2ba829ac8d869c1e4e4ee232e7ee4
Virgil Donaldson <vdonldsn@gmail.com>
2016-10-21 16:23:03 -0400

    Only addidn on e text line for checking

--- virgil/Testing1.java
+++ virgil/Testing1.java
@@ -3,5 +3,7 @@ package com.fannie;
 public class Testing1 {
 	public static void main(String[] args) {
 		System.out.print("Welcome to Java!!!");
+		System.out.print("Just wanting to add one more line!!!");
+		
 	}
 }
